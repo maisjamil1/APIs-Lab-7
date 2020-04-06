b10c56bb5838a1a527ff5bc546381b0cfbbec4e9
maisjamil1 <maisjamil17118@gmail.com>
2020-04-06 22:20:48 +0300

    3

--- server.js
+++ server.js
@@ -9,21 +9,13 @@ const superagent= require('superagent')
 app.use(cors());
 
 app.get('/location', locationHandler);
-//app.get('/weather', weatherHandler);
+app.get('/weather', weatherHandler);
 
 
 
 
 function locationHandler(request, response) {
-// app.get('/location', (request, response) => {
-  // try {
-  //   const geoData = require('./data/geo.json');
-  //   const city = request.query.city;
-  //   const locationData = new Location(city, geoData);
-  //   response.status(200).json(locationData);
-  // } catch (error) {
-  //   errorHandler(error, request, response);
-  // }});
+
   const city = request.query.city; // query string (what we will enter in the front end)
   superagent(
     `https://eu1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${city}&format=json`
@@ -50,50 +42,24 @@ function Location(city, geoData) {
 
 //____________________
 
-
-
-
-// app.get('/weather', (request, response) => {
-  // try {
-    
-  //   const weatherDateData = require('./data/darksky.json');
-  //   let DateWeatherArr=[];
-  //   for(let i=0;i<weatherDateData.data.length;i++){
-  //     let dateAndWeatherObj$$ = new weather(weatherDateData,i);
-  //     DateWeatherArr.push(dateAndWeatherObj$$);
-  // }
-  // response.send(DateWeatherArr);
-  // } catch (error) {
-  //   errorHandler(error, request, response);
-  // }});
-
-  // commented 2
-  // superagent(
-  //   `https://api.weatherbit.io/v2.0/forecast/daily?city=${request.query.search_query}&key=${process.env.WEATHER_API_KEY}`
-  // )
-  //   .then((weatherRes) => {
-  //     console.log(weatherRes);
-  //     const weatherSummaries = weatherRes.body.data.map((day) => {
-  //       return new Weather(day);
-  //     });
-  //     response.status(200).json(weatherSummaries);
-  //   })
-  //   .catch((err) => errorHandler(err, request, response))
-
-
-
-
-
-//commented 3   
-  // function weather(fileDarkSky,indexx) {
-  //   this.time =new Date(fileDarkSky.data[indexx].valid_date).toDateString();
-  //   this.forecast = fileDarkSky.data[indexx].weather.description;
-  //   weather.all.push(this);
+  function weatherHandler(request, response) {
+    superagent(
+      `https://api.weatherbit.io/v2.0/forecast/daily?city=${request.query.search_query}&key=${process.env.WEATHER_API_KEY}`
+    )
+      .then((weatherRes) => {
+        console.log(weatherRes);
+        const weatherSummaries = weatherRes.body.data.map((day) => {
+          return new Weather(day);
+        });
+        response.status(200).json(weatherSummaries);
+      })
+  }
   
-  // }
-  // weather.all=[];
-
   
+  function Weather(day) {
+    this.forecast = day.weather.description;
+    this.time = new Date(day.valid_date).toString().slice(0, 15);
+  }
 
 
 
